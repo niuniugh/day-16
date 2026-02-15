e5205c0e30c64038f05f60ad7efe04ec8c0e9047
niuniu <wisnu.jaladara@yahoo.com>
2026-02-15 22:47:31 +0700

    init repo

---
+++ apps/admin/vite.config.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+
+const config = defineConfig({
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	plugins: [
+		devtools({
+			eventBusConfig: {
+				port: 42070,
+			},
+		}),
+		// this is the plugin that enables path aliases
+		viteTsConfigPaths({
+			projects: ["./tsconfig.json"],
+		}),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+	],
+});
+
+export default config;
