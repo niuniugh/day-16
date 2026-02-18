8a9086a82cda6fa8934fa52f3916a583779d30e4
Grha Gandana P <grhagandanap@gmail.com>
2026-02-18 15:25:50 +0700

    adding login function

--- apps/api/src/modules/auth/route.ts
+++ apps/api/src/modules/auth/route.ts
@@ -1,33 +1,68 @@
-import { Hono } from "hono";
-import { prisma } from "../../utils/prisma";
-import { HTTPException } from "hono/http-exception";
-import { hashPassword } from "./utils";
 import { zValidator } from "@hono/zod-validator";
-import { RegisterSchema } from "./schema";
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { prisma } from "../../utils/prisma";
+import { LoginSchema, RegisterSchema } from "./schema";
+import { hashPassword } from "./utils";
+import { comparePassword } from "./utils";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
+export const authRouter = new Hono().post(
+	"/register",
+	zValidator("json", RegisterSchema),
+	async (c) => {
 
-export const authRouter = new Hono().post("/register", zValidator("json", RegisterSchema),async (c) => {
+		// check collision
+		const body = await c.req.valid("json");
+		const existingUser = await prisma.user.findUnique({
+			where: {
+				email: body.email,
+			},
+		});
+
+		if (existingUser) {
+			throw new HTTPException(409, { message: "Email already in use" });
+		}
+
+		// create new user using hashed password
+		const userPassword = await hashPassword(body.password);
+		await prisma.user.create({
+			data: {
+				full_name: "",
+				email: body.email,
+				password: userPassword,
+			},
+		});
+
+		return c.json({ message: "User registered successfully" });
+	},
+).post("/login", zValidator("json", LoginSchema), async (c) => {
+
     // check collision
-    const body = await c.req.json();
+    const body = await c.req.valid('json');
     const existingUser = await prisma.user.findUnique({
         where: {
             email: body.email,
         },
     });
 
-    if (existingUser) {
-        throw new HTTPException(409, { message: "Email already in use" });
+    if (!existingUser) {
+        throw new HTTPException(401, { message: "Invalid email or password" });
     }
 
-    // create new using hashed password
-    const userPassword = await hashPassword(body.password); 
-    await prisma.user.create({
-        data: {
-            full_name: '',
-            email: body.email,
-            password: userPassword,
-        },
-    });
+    // compare password
+    const isPasswordValid = await comparePassword(body.password, existingUser.password);
 
-    return c.json({ message: "User registered successfully" });
+    if (!isPasswordValid) {
+        throw new HTTPException(401, { message: "Invalid email or password" });
+    }
+
+    // generate JWT token
+    const token = jwt.sign({
+        sub: existingUser.id,
+        email: existingUser.email,
+    }, process.env.JWT_SECRET!);
+
+    return c.json({ message: "Login successful", token });
 });
--- apps/api/src/modules/auth/schema.ts
+++ apps/api/src/modules/auth/schema.ts
@@ -1,6 +1,11 @@
 import z from "zod";
 
 export const RegisterSchema = z.object({
-    email: z.email(),
-    password: z.string().min(6, "Password must be at least 6 characters long"),
+	email: z.email(),
+	password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+export const LoginSchema = z.object({
+	email: z.email(),
+	password: z.string().min(6, "Password must be at least 6 characters long"),
 });
